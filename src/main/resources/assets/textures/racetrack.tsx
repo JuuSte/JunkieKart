95d37ce1c20e14dc80ed3726aa5c2898d967c3e6
Julius <steinerjulius1@gmail.com>
2026-03-25 10:41:12 +0100

    map angefangt geht noch ncht v2

---
+++ src/main/resources/assets/textures/racetrack.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="racetrack" tilewidth="32" tileheight="32" tilecount="5184" columns="72">
+    <image source="textures/racetrack.png" width="2304" height="2304"/>
+</tileset>
